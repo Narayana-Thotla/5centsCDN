51e0b3281925abe2dbf3e7161440e2e89f851370
Thotla Lakshmi Narayana <lakshminarayanathotla@gmail.com>
2025-01-23 19:16:12 +0530

    7th

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 // import Image from "next/image";
 // import { DEFAULT_MAX_VERSION } from "tls";
 import { useRouter } from "next/navigation";
@@ -7,7 +7,8 @@ export default function Home() {
   const router = useRouter();
   router.push("portfolio/project");
   return (
-<div>hello hi there</div>
-
+    <div className="text-center text-3xl font-bold underline ">
+      welcome to shared page
+    </div>
   );
 }
